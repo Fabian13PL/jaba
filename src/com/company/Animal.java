00a69dda2d243cc0c076093b68edffd79c102495
Fabian <fabian13pl@gmail.com>
2020-09-20 15:17:14 +0200

    Start

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,39 @@
 package com.company;
 
 public class Animal {
+    final String species;
+    private Double weight;
+    boolean life = true;
+
+    public Animal(String species) {
+        this.species=species;
+        switch (species){
+            case "Lion":
+                weight=100.0;
+                break;
+            case "Dog":
+                weight=10.0;
+                break;
+            case "Cat":
+                weight=5.0;
+                break;
+        }
+    }
+
+    void feed(){
+        if(life) {
+            weight += 1.0;
+            System.out.println("You feed this "+species);
+        }else {
+            System.out.println("This "+species+" is dead, you can't feed him");
+        }
+    }
+    void takeForAWalk(){
+        if(life) {
+            weight -= 0.5;
+            System.out.println("You take for a walk "+species);
+        }else {
+            System.out.println("This "+species+" is dead");
+        }
+    }
 }
