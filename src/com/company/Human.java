257795ad4c6240f063ecebcc554ab104ee074190
Fabian <fabian13pl@gmail.com>
2020-09-20 15:36:01 +0200

    commit 4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -2,4 +2,20 @@ package com.company;
 
 public class Human {
     Animal pet = new Animal("Lion");
+
+    private Double salary;
+
+    public Double getSalary(){
+        return salary;
+    }
+    public void setSalary(Double salary){
+        if(salary<0){
+            System.out.println("Error");
+        }else {
+            System.out.println("new data has been marked for the accounting system");
+            System.out.println("be sure to pick up the annex to the contract from Mrs. Hania from human resources");
+            System.out.println("ZUS and US already know about the payment change and it makes no sense to hide your income");
+            this.salary=salary;
+        }
+    }
 }
